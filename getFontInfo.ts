e795cba26f6781afbf8b3c0baeae0faf0effc3b2
scroobius-pip <sim04ful@gmail.com>
2020-10-17 21:57:52 +0100

    included lineheight fallbacks, and size

--- getFontInfo.ts
+++ getFontInfo.ts
@@ -8,31 +8,93 @@ interface FontMap {
     [fontName: string]: FontWeights;
 }
 
-interface FontSrcMap {
-    [fontName: string]: {
-        src: string;
-        parentPath: string;
-    };
+
+interface FontVariant {
+    fallbacks: string[]
+    weight: string;
+    lineHeight: string;
+    size: string;
+}
+
+interface FontData {
+    fontName: string
+    src: SrcObj;
+    variants: Array<FontVariant>;
+}
+
+interface FontObj {
+    [elementName: string]: Array<FontData>
+}
+
+
+type SrcObj = {
+    [type in SrcTypes]: string;
+};
+
+
+enum SrcTypes {
+    eot = 'eot',
+    ttf = 'ttf',
+    otf = 'otf',
+    woff2 = 'woff2',
+    woff = 'woff',
+    other = 'other'
 }
 
 
 
-async function getFontAndSrcMaps(websiteUrl: string, isDev: boolean): Promise<{ fontMap: FontMap; fontSrcMap: FontSrcMap; }> {
+
+
+async function getFontAndSrcMaps(websiteUrl: string, isDev: boolean): Promise<FontObj> {
     const browser = await getBrowser(isDev);
     await browser.goto(websiteUrl, { timeout: 0 });
 
     return await browser.evaluate(() => {
 
+
+
+        const selectFontSrc = (font: SrcObj) => {
+            return font?.ttf ?? font?.otf ?? font?.eot ?? font?.woff ?? font?.woff2 ?? Object.values(font)[0];
+        }
+
+        function getSrcExtension(s: string) {
+            return s.split('.').pop();
+        }
+
+        function getSrcObjName(extension: string) {
+            return extension.length > 5 ? 'other' : extension;
+        }
+
+        function extractFontUrls(s: string): string[] {
+            if (!s) return []
+
+            const srcs = s.split(',');
+            // return srcs
+            return srcs.map(src => {
+                const urls = src.match(/(?<=")(.*\.(ttf|woff2|woff|otf|eot))/g);
+                if (urls) {
+                    return urls;
+                }
+            }).filter(Boolean).flat();
+
+        };
+
         function getFontSrcMap(tidyFontName: (font: string) => string, getParentPath: (url: string) => string) {
-            const map = new Map<string, { src: any; parentPath: string; }>();
+            type FontName = string
+            type FontSrc = { src: any; parentPath: string; }
+
+            const map = new Map<FontName, FontSrc>();
 
             const documentStylesheets = [...document.styleSheets];
             documentStylesheets.forEach(documentStylesheet => {
                 const cssRules = [...documentStylesheet.cssRules];
+
+
                 cssRules.forEach(cssRule => {
                     if (cssRule instanceof CSSFontFaceRule) {
+
                         map.set(tidyFontName(cssRule.style.fontFamily), {
-                            src: ((cssRule as any).style?.src),
+                            src: ((cssRule as any).style?.src) as string,
                             parentPath: getParentPath(cssRule.parentStyleSheet.href)
                         });
                     }
@@ -43,7 +105,7 @@ async function getFontAndSrcMaps(websiteUrl: string, isDev: boolean): Promise<{ 
                         nestedCssRules.forEach(rule => {
                             if (rule instanceof CSSFontFaceRule) {
                                 map.set(tidyFontName(rule.style.fontFamily), {
-                                    src: ((rule as any).style?.src),
+                                    src: ((rule as any).style?.src) as string,
                                     parentPath: getParentPath(rule.parentStyleSheet.href)
                                 });
                             }
@@ -56,29 +118,52 @@ async function getFontAndSrcMaps(websiteUrl: string, isDev: boolean): Promise<{ 
         }
 
 
-        function getFontMap(nodes: Element[], tidyFontName: (font: string) => string) {
-            const fontMap = new Map<string, string[]>();
+        const initGetElementFontData = () => {
+            const srcMap = getFontSrcMap(tidyFontName, getParentPath)
 
+            return (element: Element): FontData[] => {
+                type FontName = string
+                const fontMap = new Map<FontName, Array<FontVariant>>();
 
-            const pseudoElements = ['', ':before', ':after'];
-            nodes.forEach(node => {
+                const getFontVariant = ({ fontWeight, lineHeight, fontSize }: CSSStyleDeclaration, fallbacks: string[]): FontVariant => ({ lineHeight, size: fontSize, weight: fontWeight, fallbacks })
+
+                const pseudoElements = ['', ':before', ':after'];
+
                 pseudoElements.forEach(pseudo => {
-                    const { fontFamily, fontWeight } = window.getComputedStyle(node, pseudo);
-                    // console.log(fontFamily)
-                    if (!fontFamily)
-                        return null;
 
-                    fontFamily.split(/\n*,\n*/g).forEach(font => {
-                        const fontName = tidyFontName(font);
-                        if (fontMap.has(fontName)) {
-                            fontMap.set(fontName, [...fontMap.get(fontName), fontWeight]);
-                        } else {
-                            fontMap.set(fontName, [fontWeight]);
-                        }
-                    });
+                    const elementStyle = window.getComputedStyle(element, pseudo);
+
+
+                    if (!elementStyle?.fontFamily)
+                        return
+
+                    const [fontName, ...fallbacks] = elementStyle?.fontFamily.split(/\n*,\n*/g).map(tidyFontName)
+
+                    if (fontMap.has(fontName)) {
+                        fontMap.set(fontName, [...fontMap.get(fontName), getFontVariant(elementStyle, fallbacks)]);
+                    } else {
+                        fontMap.set(fontName, [getFontVariant(elementStyle, fallbacks)]);
+                    }
+
                 });
-            });
-            return fontMap;
+
+
+
+                return (() => {
+                    const fontDatum = [] as FontData[]
+                    fontMap.forEach((variants, fontName) => {
+                        const { parentPath = '', src = '' } = srcMap.get(fontName)
+                        const srcArray = extractFontUrls(src)
+                        const srcObj = srcArray.reduce((srcObj, s) => {
+                            const extension = getSrcExtension(s)
+                            srcObj[getSrcObjName(extension)] = convertRelativeToAbsolute(parentPath, s)
+                            return srcObj
+                        }, {} as SrcObj)
+                        fontDatum.push({ fontName, src: srcObj, variants })
+                    })
+                    return fontDatum
+                })()
+            }
         }
 
 
@@ -87,100 +172,32 @@ async function getFontAndSrcMaps(websiteUrl: string, isDev: boolean): Promise<{ 
         }
 
         function getAllNodes() {
-            return Array.from(window.document.getElementsByTagName('*'));
+            const tagNames = ['p', 'span', 'h1', 'h2', 'h3', 'h4', 'h5', 'a', 'button', 'strong',]
+            return tagNames.map(t => {
+                const elements = window.document.getElementsByTagName(t)
+                return Array.from(elements)
+            }).flat()
+
         }
 
         const getParentPath = (url: string | null) => (url ?? '').substring(0, (url ?? '').lastIndexOf("/"));
 
+        const elements = getAllNodes();
+        const getElementFontData = initGetElementFontData()
 
-        const nodes = getAllNodes();
-        const fontMap = getFontMap(nodes, tidyFontName);
-        const fontSrcMap = getFontSrcMap(tidyFontName, getParentPath);
-        return { fontMap: Object.fromEntries(fontMap), fontSrcMap: Object.fromEntries(fontSrcMap) };
-    });
+        const fontMap = elements.reduce((map, element) => {
+            const fontData = getElementFontData(element)
+            const elementName = element.tagName
+            if (map.has(elementName)) {
+                map.set(elementName, [...map.get(elementName), ...fontData])
+            } else {
+                map.set(elementName, fontData)
+            }
+            return map
+        }, new Map<string, Array<FontData>>())
 
-
-
+        return Object.fromEntries(fontMap)
+    })
 }
 
-
-async function mergeFontAndSrcMap(fontMap: FontMap, fontSrcMap: FontSrcMap) {
-
-    interface FontObj {
-        [fontName: string]: {
-            weights: string[],
-            src: SrcObj,
-            svg: string
-        } | undefined
-    }
-
-    type SrcObj = {
-        [type in SrcTypes]: string;
-    };
-
-
-    // type SrcTypes = 'eot' | 'ttf' | 'otf' | 'woff' | 'woff2'
-    enum SrcTypes {
-        eot = 'eot',
-        ttf = 'ttf',
-        otf = 'otf',
-        woff2 = 'woff2',
-        woff = 'woff',
-        other = 'other'
-    }
-
-    const selectFontSrc = (font: SrcObj) => {
-        return font?.ttf ?? font?.otf ?? font?.eot ?? font?.woff ?? font?.woff2 ?? Object.values(font)[0];
-    }
-
-    let fontObj: FontObj = {};
-    for (const fontName in fontMap) {
-        const { src, parentPath: href } = fontSrcMap?.[fontName] ?? { src: '', parentPath: '' };
-        fontObj[fontName] = {
-            weights: Array.from(new Set(fontMap[fontName])),
-            ...await (async () => {
-                // const srcArray = Array.from(getUrls(src))
-                const srcArray = extractFontUrls(src)
-                // console.log(srcArray)
-                // console.log(src)
-                const srcObj = srcArray.reduce((srcObj, s) => {
-
-                    const extension = s.split('.').pop()
-
-                    srcObj[extension.length > 5 ? 'other' : extension] = convertRelativeToAbsolute(href, s)
-                    return srcObj
-                }, {} as SrcObj)
-
-                const fontSrc = selectFontSrc(srcObj)
-
-                const fontPath = await convertFontToPath(fontSrc, 'Hello')
-
-
-                return { src: srcObj, svg: fontPath }
-            })()
-            ,
-            // href
-        };
-    }
-    return fontObj;
-
-}
-function extractFontUrls(s: string): string[] {
-    if (!s) return []
-
-    const srcs = s.split(',');
-    // return srcs
-    return srcs.map(src => {
-        const urls = src.match(/(?<=")(.*\.(ttf|woff2|woff|otf|eot))/g);
-        if (urls) {
-            return urls;
-        }
-    }).filter(Boolean).flat();
-
-};
-
-export default async (websiteUrl: string, isDev: boolean) => {
-    const { fontSrcMap, fontMap } = await getFontAndSrcMaps(websiteUrl, isDev)
-
-    return mergeFontAndSrcMap(fontMap, fontSrcMap)
-}
+export default async (websiteUrl: string, isDev: boolean) => await getFontAndSrcMaps(websiteUrl, isDev)
